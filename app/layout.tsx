9cfef563dbb3fda3d66e5bd2c9c4c34ed9472a81
sonyerg <ericksonrguinto@gmail.com>
2024-06-13 10:01:54 +1200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+import "./globals.css";
+import { Footer } from "@/components/footer";
+import { NavBar } from "@/components/navbar";
+import { Container } from "@/components/ui/container";
+
+const font = Urbanist({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Store",
+  description: "Store front page",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ components/main-nav.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Category } from "@/types";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface MainNavProps {
+  data: Category[];
+}
+
+export function MainNav({ data }: MainNavProps) {
+  const pathname = usePathname();
+
+  const routes = data.map((route) => ({
+    href: `/category/${route.id}`,
+    label: route.name,
+    active: pathname === `/category/${route.id}`,
+  }));
+
+  return (
+    <nav className="mx-6 flex items-center space-x-4 lg:space-6">
+      {routes.map((route) => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-black",
+            route.active ? "text-black dark:text-white" : "text-neutral-500"
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </nav>
+  );
+}
