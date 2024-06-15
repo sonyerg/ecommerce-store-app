7ba7bc70deaf0c56b2d8de198c4244a4efefd799
sonyerg <ericksonrguinto@gmail.com>
2024-06-16 08:39:34 +1200

    added bg-center

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -8,7 +8,7 @@ export function Billboard({ data }: BillboardProps) {
   return (
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
       <div
-        className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
+        className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover bg-center"
         style={{ backgroundImage: `url(${data?.imageUrl})` }}
       >
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
